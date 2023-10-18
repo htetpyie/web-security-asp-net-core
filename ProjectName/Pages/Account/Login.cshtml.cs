72c115938211db544bfe7a707e41678ce41d7ead
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2023-10-18 23:08:04 +0630

    -

--- ProjectName/Pages/Account/Login.cshtml.cs
+++ ProjectName/Pages/Account/Login.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Security.Claims;
 
 namespace ProjectName.Pages.Account
 {
@@ -13,15 +15,35 @@ namespace ProjectName.Pages.Account
         {
         }
 
-        public void OnPost()
+        public async Task<IActionResult> OnPost()
         {
+            if (!ModelState.IsValid) return Page();
 
+            //Verity the Credential
+            if (Credential.UserName == "admin" && Credential.Password == "admin")
+            {
+                //Creating a security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,"admin"),
+                    new Claim(ClaimTypes.Email, "admin@gmail.com"),
+                };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+            return Page();
         }
     }
     public class Credential
     {
         [Required]
-        [Display(Name ="User Name")]
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
 
         [Required]
